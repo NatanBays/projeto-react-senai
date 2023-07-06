bcf8b72b0c5fa3b4e65d0acd4962d0931bc22288
FabricioFLS <fabricioluizsouza@gmail.com>
2023-07-06 18:34:34 -0300

    Maps

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Home from './pages/home/Home';
 import Product from './pages/product/Product';
 import Info from './pages/info/Info';
 import Contact from './pages/contact/Contact';
+import Localization from './pages/localization/Localization';
 import React from 'react';
 import Footer from "./components/footer/Footer";
 
@@ -24,6 +25,9 @@ function App() {
           <li>
             <Link to="/contact">Contato</Link>
           </li>
+          <li>
+            <Link to="/localization">Localização</Link>
+          </li>
         </ul>
       </nav>
       <Routes>
@@ -31,6 +35,7 @@ function App() {
         <Route path="/info" Component={Info}/>
         <Route path="/product" Component={Product}/>
         <Route path="/contact" Component={Contact}/>
+        <Route path="/localization" Component={Localization}/>
       </Routes>
       <Footer/>
     </Router>
